41c9caf9165be3eb3dca0c98afe8943ea5f69a98
sapradhan12 <sanaypradhan@gmail.com>
2023-11-03 20:52:39 -0500

    improved ordering

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
+
 openShopping.addEventListener('click', ()=>{
     body.classList.add('active');
 })
@@ -17,37 +18,37 @@ let products = [
     {
         id: 1,
         name: 'Game Day Grill Classic Burger',
-        image: 'classic.png',
+        image: 'classic-burger.png',
         price: 11.99
     },
     {
         id: 2,
         name: 'BBQ Pulled Pork Sandwich',
-        image: '',
+        image: 'pulled-pork-sandwich.png',
         price: 11.99
     },
     {
         id: 3,
         name: 'Spicy Chicken Wings',
-        image: '',
+        image: 'spicy-chicken-sandwich.png',
         price: 12.99
     },
     {
         id: 4,
         name: 'Grilled Steak Tacos',
-        image: '',
-        price: 13.99
+        image: 'grilled-steak-tacos.png',
+        price: 10.99
     },
     {
         id: 5,
-        name: '',
-        image: '',
-        price: 14.99
+        name: 'Pan-Seared Steak',
+        image: 'steak.png',
+        price: 20.99
     },
     {
         id: 6,
-        name: '',
-        image: '',
+        name: 'Oven-Baked Ribs',
+        image: 'ribs.png',
         price: 16.99
     }
 ];
@@ -60,14 +61,14 @@ function initApp(){
             <img src="images/${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">Add To Card</button>`;
+            <button onclick="addToCard(${key})">Add To Order</button>`;
         list.appendChild(newDiv);
     })
 }
 initApp();
 function addToCard(key){
     if(listCards[key] == null){
-        // copy product form list to list card
+        // copy product from list to list card
         listCards[key] = JSON.parse(JSON.stringify(products[key]));
         listCards[key].quantity = 1;
     }
